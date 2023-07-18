8c4e9b505a2017b1143860b704b14fbf41736696
kwonjs9804 <kwonjs9804@naver.com>
2023-07-18 16:46:03 +0900

    edit source

--- Colorspace.py
+++ Colorspace.py
@@ -4,10 +4,9 @@
 # git commit -m "edit soruce"
 # git push origin main
 
-# import cv2
+import cv2
 
-# image = cv2.imread(
-#     "/Users/j_siki/Desktop/HYU/Programming/Colorspace/lcdrgb.jpg", cv2.IMREAD_GRAYSCALE)
-# cv2.imshow("Test", image)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+image = cv2.imread("lcdrgb", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Test", image)
+cv2.waitKey()
+cv2.destroyAllWindows()
